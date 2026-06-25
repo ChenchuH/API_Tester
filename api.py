411d0d1185bcb4b895ada2c8d3d8f07b972ad2a0
ChenchuH <ynchenchuh@icloud.com>
2026-06-24 22:15:16 -0700

    cleaned up code and included error checking

--- api.py
+++ api.py
@@ -5,6 +5,15 @@ import json
 method, url = sys.argv[1], sys.argv[2]
 
 if method == "GET":
-    r = requests.get(url)
-    print(f"Status Code: {r.status_code}")
-    print(json.dumps(r.json(), indent=2))
+    try:
+        r = requests.get(url)
+        print(f"Status Code: {r.status_code}\n")
+        try:
+            print(json.dumps(r.json(), indent=2))
+
+        except ValueError:
+            print("Response is not valid JSON")
+            print(f"Response content: {r.text}")
+
+    except requests.RequestException as e:
+        print(f"Error: {e}")
